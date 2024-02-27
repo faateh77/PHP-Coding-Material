1b383bba71e142668433c3d372b6e5a7f43eb551
noor uddin muhammad faateh <52114434+faateh77@users.noreply.github.com>
2024-02-27 14:12:16 +0500

    Add files via upload

---
+++ task2.php
@@ -0,0 +1,27 @@
+<?php
+//Find the area of triangle,square,rectangle,circle
+
+$pi = 3.141;
+//Area Of Circle 
+$r = 2;
+$areaofcircle = $pi * $r;
+echo "Area of circle is" . " " . $areaofcircle .  "<br>";
+
+//Area Of Rectangle
+$l = 3;
+$w = 5; 
+$areaofrectangle = $l * $w;
+echo "Area of rectangle is" . " " . $areaofrectangle .  "<br>";
+
+//Area Of square
+$a = 2;
+$areaofsquare = $a * $a;
+echo "Area of square is" . " " . $areaofsquare .  "<br>";
+
+//Area Of triangle
+$h = 2;
+$b = 3;
+$areaoftriangle = $h * $b;
+echo "Area of triangle is" . " " . $areaoftriangle .  "<br>";
+
+?>
