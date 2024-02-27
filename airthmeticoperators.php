7dd9f44347a5186a743a5e283c017395011afb95
noor uddin muhammad faateh <52114434+faateh77@users.noreply.github.com>
2024-02-27 12:17:06 +0500

    Add files via upload

---
+++ airthmeticoperators.php
@@ -0,0 +1,33 @@
+<?php
+
+//Addition
+$num1 = 5;
+$num2 = 10;
+$sum = $num1 + $num2;
+echo $sum . "<br>";
+
+//Subtraction
+$num3 = 10;
+$num4 = 5;
+$diff = $num3 - $num4;
+echo $diff . "<br>";
+
+//Multiplication
+$num5 = 2;
+$num6 = 3;
+$mul = $num5 * $num6;
+echo $mul . "<br>";
+
+//Division
+$num7 = 8;
+$num8 = 2;
+$div = $num7/$num8;
+echo $div . "<br>";
+
+
+
+
+
+
+
+?>
