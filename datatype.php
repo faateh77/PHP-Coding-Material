8d8f13ee51432b01cf70f1ac08bea1855d8a7386
noor uddin muhammad faateh <52114434+faateh77@users.noreply.github.com>
2024-02-27 12:14:30 +0500

    Add files via upload

---
+++ datatype.php
@@ -0,0 +1,31 @@
+<?php
+//integer
+
+$num = 12;
+
+echo $num . "<br>";
+
+//String
+
+$name = "rehan" ;
+echo $name . "<br>";
+
+//Boolean
+
+$boolean = true;
+$false = false;
+
+echo $boolean . "<br>";
+echo $false . "<br>";
+
+//Float
+
+$float = 3.4;
+echo $float . "<br>";
+
+
+
+
+
+
+?>
