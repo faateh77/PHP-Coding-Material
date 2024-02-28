d6b6ce70777faac92d4abccca83cd1ccf69537be
noor uddin muhammad faateh <52114434+faateh77@users.noreply.github.com>
2024-02-29 02:48:57 +0500

    Add files via upload

---
+++ comparisonoperator.php
@@ -0,0 +1,20 @@
+<?php
+
+
+$num1  = 224;
+$num2 = 41;
+// if($num1 == $num2){
+//      echo "numbers are equal";
+// }
+// else{
+//     echo "numbers are not equal";
+// }
+
+if($num1 > $num2){
+    echo "number 1 is greater than number2";
+}
+else{
+   echo "number2 is greater than number1";
+}
+
+?>
