e68de22acf5ccb1c153ef0c33faa446373f5de8c
noor uddin muhammad faateh <52114434+faateh77@users.noreply.github.com>
2024-02-29 02:30:06 +0500

    Add files via upload

--- concatenation.php
+++ concatenation.php
@@ -2,7 +2,10 @@
 
 $firstName = "John";
 $lastName = "Doe";
-echo 'My full name is' . " " . $firstName . " " . $lastName;
+echo 'My full name is' . " " . $firstName . " " . $lastName . "<br>";
+
+
+echo "this is my  first name" . " " . $firstName ; 
 
 
 
