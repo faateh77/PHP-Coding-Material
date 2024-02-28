14181fa6f8b662d0bc13baf11bfab953b227f060
noor uddin muhammad faateh <52114434+faateh77@users.noreply.github.com>
2024-02-28 13:53:28 +0500

    Add files via upload

---
+++ conditional statements/ifelseif.php
@@ -0,0 +1,22 @@
+<?php
+$age = 45;
+if ($age <= 12){
+    echo "you are child";
+}
+else if ($age < 20){
+    echo "you are a teenager";
+}
+else if ($age >= 21 && $age <=50){
+    echo "you are an Adult";
+}
+else{
+    echo "you are Elderly Person";
+}
+
+
+
+
+
+
+
+?>
