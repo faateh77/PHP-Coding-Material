0ce00b22b1d3fd50d5ec720dd633dbeba7a24464
noor uddin muhammad faateh <52114434+faateh77@users.noreply.github.com>
2024-03-04 15:07:25 +0500

    Add files via upload

---
+++ logicaloperator.php
@@ -0,0 +1,31 @@
+<?php
+
+$a = 4;
+$b = 4;
+$c = 4;
+//OR
+// if($a == $b OR $a == $c){
+//     echo "true";
+// }
+// else{
+//     echo "false";
+// }
+//AND 
+// if($a == $b AND $a == $c){
+//     echo "true";
+// }
+// else{
+//     echo "false";
+// }
+//XOR
+if($a == $b XOR $a == $c){
+    echo "true";
+}
+else{
+    echo "false";
+}
+
+
+
+
+?>
