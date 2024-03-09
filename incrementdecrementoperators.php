bf6c6330372dbb44d61db977ce07d49d5f803372
noor uddin muhammad faateh <52114434+faateh77@users.noreply.github.com>
2024-03-09 17:32:48 +0500

    Add files via upload

---
+++ incrementdecrementoperators.php
@@ -0,0 +1,23 @@
+<?php
+//pre increment
+// $a = 5;
+// ++$a;
+// echo $a;
+
+//post increment
+
+// $a = 5;
+// $a++;
+// echo $a++ . "<br>";
+// echo $a++ . "<br>";
+
+// //pre decrement
+// $a = 5;
+// --$a;
+// echo $a;
+
+//Post Decrement
+$a = 5;
+echo $a--;
+
+?>
