bb20620d71c4f7dcbcfd35fa02cfb899d12dafb7
noor uddin muhammad faateh <52114434+faateh77@users.noreply.github.com>
2024-04-03 07:00:43 +0500

    Add files via upload

---
+++ ternaryoperator.php
@@ -0,0 +1,14 @@
+<?php
+
+$a = 5;
+
+// if($a == 5){
+//     echo "yes it is equal";
+// }
+// else{
+//     echo "no its not equal";
+// }
+
+($a==5) ? "yes it is equal" : "no its not equal";
+
+?>
