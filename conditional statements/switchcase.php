91976c0a1e608bd3e1349e2906a30e85e599e888
noor uddin muhammad faateh <52114434+faateh77@users.noreply.github.com>
2024-04-03 17:41:30 +0500

    Add files via upload

---
+++ conditional statements/switchcase.php
@@ -0,0 +1,19 @@
+<?php
+$favColor = "green";
+switch($favColor){
+    case "blue":
+        echo "your fovourite color is" . " " . $favColor;
+        break;
+    case "red":
+        echo "your favourite color is" . " " . $favColor;
+        break;
+    case "green":
+        echo "your favourite color is" . " " . $favColor;
+        break;
+
+    default:
+        echo "I am not sure about your favorite color";
+
+    }
+
+?>
