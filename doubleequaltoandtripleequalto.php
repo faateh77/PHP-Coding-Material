5e60f91273fca0c95421014146d82e7e7c396bcf
noor uddin muhammad faateh <52114434+faateh77@users.noreply.github.com>
2024-04-04 07:46:41 +0500

    Add files via upload

---
+++ doubleequaltoandtripleequalto.php
@@ -0,0 +1,22 @@
+<?php
+
+$num1 = 4;
+$num2  ="4";
+//double equal to
+if($num1 == $num2){
+    echo "The values are same";
+}
+else{
+    echo "The values are same but data types are different";
+}
+//triple equal to
+if($num1 === $num2){
+    
+    echo "The values are same";
+}
+else{
+    echo "The values are same but data types are different";
+}
+
+
+?>
