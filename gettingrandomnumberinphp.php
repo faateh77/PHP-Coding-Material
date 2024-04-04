408ab72ad1e9c60648ea16721e2e1f221f8e4598
noor uddin muhammad faateh <52114434+faateh77@users.noreply.github.com>
2024-04-04 07:58:17 +0500

    Add files via upload

---
+++ gettingrandomnumberinphp.php
@@ -0,0 +1,11 @@
+<?php
+
+$min = 0;
+$max = 100;
+$randomNumber = rand($min,$max);
+
+
+echo "Random Number from". " " .  $min . " " . "till" . " " . $max . " ".  "is:" . " " .$randomNumber;
+
+
+?>
